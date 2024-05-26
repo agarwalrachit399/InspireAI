44e94b469d5a116bee46af519bfe976e456aed5f
Rachit <agarwalrachit399@gmail.com>
2024-05-25 19:38:13 -0700

    Fixed Typo

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,6 +6,7 @@ export const GET = async (request) => {
         await connectToDatabase();
 
         const prompts = await Prompt.find({}).populate('creator');
+        console.log(prompts)
         return new Response(JSON.stringify(prompts), {status:200})
     } catch(error){
         return new Response("Failed to fetch all Prompts", {status:500})
